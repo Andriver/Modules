f6e6cfc52f036f15301a201d6fac385f9cc3155c
Andriver <aprudnik@gmail.com>
2023-03-18 00:17:29 +0200

    Added java cross platform file path

--- src/main/java/module10/TestModule10.java
+++ src/main/java/module10/TestModule10.java
@@ -5,14 +5,14 @@ public class TestModule10 {
     public static void main(String[] args) {
 
         PhoneValidator phoneValidator = new PhoneValidator();
-        phoneValidator.validateFile("data//file.txt");
+        phoneValidator.validateFile("/GoIT/Java/Modules/src/main/java/module10/data/file.txt");
 
-        List<User> users = User.downloadUsersFromFile("data//users.txt");
-        FilesUtils.saveTextFile("data//user.json", User.toJson(users));
+        List<User> users = User.downloadUsersFromFile("/GoIT/Java/Modules/src/main/java/module10/data/users.txt");
+        FilesUtils.saveTextFile("/GoIT/Java/Modules/src/main/java/module10/data/user.json", User.toJson(users));
 
         System.out.println("-----------------------------------");
 
-        WordsUtils.countWordInFile("data//words.txt");
+        WordsUtils.countWordInFile("/GoIT/Java/Modules/src/main/java/module10/data/words.txt");
 
     }
 }
