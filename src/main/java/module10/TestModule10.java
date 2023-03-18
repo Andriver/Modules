59eea06e8af5d4339a72264faed52c3a5e9aeac9
Andriver <aprudnik@gmail.com>
2023-03-18 14:50:42 +0200

    Added Maven dependency to pom.xml

--- src/main/java/module10/TestModule10.java
+++ src/main/java/module10/TestModule10.java
@@ -5,14 +5,14 @@ public class TestModule10 {
     public static void main(String[] args) {
 
         PhoneValidator phoneValidator = new PhoneValidator();
-        phoneValidator.validateFile("/GoIT/Java/Modules/src/main/java/module10/data/file.txt");
+        phoneValidator.validateFile("src/main/java/module10/data/file.txt");
 
-        List<User> users = User.downloadUsersFromFile("/GoIT/Java/Modules/src/main/java/module10/data/users.txt");
-        FilesUtils.saveTextFile("/GoIT/Java/Modules/src/main/java/module10/data/user.json", User.toJson(users));
+        List<User> users = User.downloadUsersFromFile("src/main/java/module10/data/users.txt");
+        FilesUtils.saveTextFile("src/main/java/module10/data/user.json", User.toJson(users));
 
         System.out.println("-----------------------------------");
 
-        WordsUtils.countWordInFile("/GoIT/Java/Modules/src/main/java/module10/data/words.txt");
+        WordsUtils.countWordInFile("src/main/java/module10/data/words.txt");
 
     }
 }
